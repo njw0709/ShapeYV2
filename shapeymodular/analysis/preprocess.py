6b5ab0cdb1bf1c3be0a477590ca6ed4eec74e671
namj <jwnam0709@gmail.com>
2023-09-05 10:40:02 -0700

    adding read and threshold function

--- shapeymodular/analysis/preprocess.py
+++ shapeymodular/analysis/preprocess.py
@@ -1,4 +1,8 @@
 import numpy as np
+from typing import List
+import tables
+from tqdm import tqdm
+from multiprocessing import Pool
 
 
 def compute_activation_threshold(
@@ -20,3 +24,30 @@ def compute_activation_threshold(
     threshold_idx = np.argmax(norm_cdf > activation_level, axis=1)
     threshold = bins_array[np.arange(bins_array.shape[0]), threshold_idx]
     return threshold
+
+
+def read_file(fname_key):
+    fname, key = fname_key
+    hf = tables.open_file(fname, "r")
+    features = []
+    for i in range(3):
+        features.append(np.array(hf.get_node("/{}_{}".format(key, i))))
+    hf.close()
+    return features
+
+
+def read_and_threshold_features(
+    data_names: List[str],
+    thresholds_list,
+    out_feature_list: List[np.ndarray],
+    feature_key: str = "l2pool",
+):
+    data_names_and_key = [(fname, feature_key) for fname in data_names]
+    with Pool(4) as pool:
+        for i, features in tqdm(
+            enumerate(pool.imap(read_file, data_names_and_key)), total=len(data_names)
+        ):
+            out_feature_list[0][:, :, i] = features[0] > thresholds_list[0]
+            out_feature_list[1][:, :, i] = features[1] > thresholds_list[1]
+            out_feature_list[2][:, :, i] = features[2] > thresholds_list[2]
+    return out_feature_list
