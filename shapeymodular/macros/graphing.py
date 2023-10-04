2caaa04bd8a469d30d3e451d3537b9a7298310d4
namj <jwnam0709@gmail.com>
2023-10-04 11:12:57 -0700

    updating gitignore

--- shapeymodular/macros/graphing.py
+++ shapeymodular/macros/graphing.py
@@ -39,20 +39,23 @@ def plot_nn_classification_error_graph(
         config = dc.load_config(
             os.path.join(feature_directory, utils.PATH_CONFIG_PW_NO_CR)
         )
+        axes = [axes_choice]
     elif axes_choice == "all":
         cmd = ["cp", utils.PATH_CONFIG_ALL_NO_CR, "."]
         utils.execute_and_print(cmd)
         config = dc.load_config(
             os.path.join(feature_directory, utils.PATH_CONFIG_ALL_NO_CR)
         )
+        axes = typing.cast(List, config.axes)
     else:
         assert config_filename is not None
         config = dc.load_config(os.path.join(feature_directory, config_filename))
+        assert axes_choice in typing.cast(List, config.axes)
+        axes = [axes_choice]
 
     analysis_hdf_path = os.path.join(feature_directory, analysis_file)
     analysis_hdf = h5py.File(analysis_hdf_path, "r")
     analysis_sampler = dl.Sampler(data_loader, analysis_hdf, config)
-    axes = typing.cast(List, config.axes)
     dict_ax_to_graph_data_group_obj = {}
     dict_ax_to_graph_data_group_cat = {}
     fig_obj, ax_obj = plt.subplots(1, 1)
