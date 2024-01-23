fixing normalization for CR, and adding cosine similarity (unnormalized dot)

shapeymodular/macros/compute_distance_torch.py
@@ -220,15 +220,22 @@ def load_features(
         else:
             data_row = typing.cast(np.ndarray, features[0])
             data_col = typing.cast(np.ndarray, features[1])
-        print("row data shape: {}".format(data_row.shape))
-        print("col data shape: {}".format(data_col.shape))
+        assert data_row.shape[1] == data_col.shape[1]
+        print("feature length: {}".format(data_row.shape[1]))
 
         data_row = torch.tensor(data_row, dtype=dtype)
         data_col = torch.tensor(data_col, dtype=dtype)
         if metric == "correlation":
             print("normalizing features...")
-            data_row = torchutilfeat.standardize_features(data_row)
-            data_col = torchutilfeat.standardize_features(data_col)
+            joined_data = torch.cat((data_row, data_col), dim=0)
+            mean_joined = joined_data.mean(dim=0, keepdim=True)
+            std_joined = joined_data.std(dim=0, keepdim=True)
+            data_row = torchutilfeat.standardize_features(
+                data_row, mean=mean_joined, std=std_joined
+            )
+            data_col = torchutilfeat.standardize_features(
+                data_col, mean=mean_joined, std=std_joined
+            )
     else:
         if isinstance(features, str):
             assert os.path.exists(features)

shapeymodular/torchutils/features.py
@@ -1,10 +1,17 @@
 import torch
+from typing import Union, Tuple
 
 
-def standardize_features(features: torch.Tensor) -> torch.Tensor:
+def standardize_features(
+    features: torch.Tensor,
+    mean: Union[torch.Tensor, None] = None,
+    std: Union[torch.Tensor, None] = None,
+) -> torch.Tensor:
     # features: (n, l)
     # output: (n, l)
-    mean = features.mean(dim=1, keepdim=True)
-    std = features.std(dim=1, keepdim=True)
+    if mean is None:
+        mean = features.mean(dim=0, keepdim=True)
+    if std is None:
+        std = features.std(dim=0, keepdim=True)
     output = (features - mean) / std
     return output